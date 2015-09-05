f18c50f3aece5125d644d01f795de930d6f05bb7
Yuri Moens <yuri.moens@gmail.com>
2015-09-05 03:05:31 +0200

    Move screenshots correctly.

--- bin/bin/screen.sh
+++ bin/bin/screen.sh
@@ -12,10 +12,10 @@ fi
 
 case "$1" in
     root)
-	scrot '%F-%T.png' -e 'mv $f $directory'
+	scrot '%F-%T.png' -e 'mv $f '$directory''
         ;;
     active)
-       	scrot -u '%F-%T.png' -e 'mv $f $directory'
+       	scrot -u '%F-%T.png' -e 'mv $f '$directory''
         ;;
     *)
         echo "Usage: $0 {root|active}"
