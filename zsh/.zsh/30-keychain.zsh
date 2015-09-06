b9c14568ad7f59975e630769221cc3515dfbcca4
Yuri Moens <yuri.moens@gmail.com>
2015-09-06 14:01:11 +0200

    Fix key name.

--- zsh/.zsh/30-keychain.zsh
+++ zsh/.zsh/30-keychain.zsh
@@ -1,3 +1,3 @@
-export SSH_KEY_PATH="~/.ssh/id_rsa"
+export SSH_KEY_PATH="~/.ssh/yuri"
 
-eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa)
+eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/yuri)
